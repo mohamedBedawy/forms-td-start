71f69695c83b1a1acefaf39ddbfd3fb49e8ce19d
Mohamed Bedawy <mohamed.bedawy@maqta.ae>
2023-01-11 21:28:54 +0400

    Form control SetValue and PatchValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,21 +6,35 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  @ViewChild('f',{static:true}) signupForm:NgForm;
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
+  @ViewChild('f', { static: true }) signupForm: NgForm;
 
-  genders =['male','female'];
+  genders = ['male', 'female'];
 
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    // const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     userName: 'Mohamed',
+    //     email:'mm@gmail.com'
+    //   },
+    //   secret:'',
+    //   QuestionAnswer:'',
+    //   gender:'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        userName: 'Mohamed'}
+
+    });
   }
 
   // onSubmit(f:NgForm){
   //   console.log('Submnitted',f)
   // }
 
-  onSubmit(){
-    console.log('Submnitted',this.signupForm)
-
+  onSubmit() {
+    console.log('Submnitted', this.signupForm);
   }
 }
